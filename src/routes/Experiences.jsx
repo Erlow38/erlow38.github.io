3181487ff431eda1847ca0f88afaebf7a14d3627
Ethan EHRLER <ethan.ehrler@etu.univ-grenoble-alpes.fr>
2023-10-20 14:42:58 +0200

    new v

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -15,7 +15,7 @@ function Experiences() {
 
                 <div className="timeline">
                     <div className="entries">
-                        <Timeline image={"../assets/logo/sercel.png"} title={"Sercel"} date={"2023-2024"} description={"Développeur Web au sein de Sercel Grenoble"} />
+                        <Timeline image={"../assets/logo/sercel.png"} title={"Sercel"} date={"2023-2024"} description={"Développeur Web au sein de Sercel Grenoble."} />
                         <Timeline image={"../assets/logo/lig.jpg"} title={"Laboratoire informatique de Grenoble (LIG)"} date={"2023"} description={"Extension de l'outil LOMET en support à l'ingénierie des méthodes (stage de 10 semaines)."} />
                         <Timeline image={"../assets/logo/IUT2.jpeg"} title={"BUT informatique"} date={"2021-2024"} description={"Formation dans le domaine de l'informatique à Grenoble au sein de l'IUT 2, UGA."} />
                     </div>
