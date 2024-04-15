79b533386fee34b403dba324ccb68227ff510b66
Erlow <erlowgaming@gmail.com>
2024-04-15 17:38:28 +0200

    fix text

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -18,7 +18,7 @@ function About() {
                         </p>
                         <p>Mon passé inclut dix années de pratique du tennis de table, deux ans de karaté, et une passion pour le ski depuis mon plus jeune âge, ayant suivi des cours dans cette discipline. De plus, je suis membre d'une association dans mon village, active dans l'organisation d'une variété d'événements et de festivités.
                         </p>
-                        <p>Vous pouvez mon contacter par mail à <a href="mailto: ethan.ehrler@outlook.com">ethan.ehrler@outlook.com</a>, par téléphone au <a href="tel: 06 16 52 07 25">06 16 52 07 25</a> ou via <a target="_blank" href="https://www.linkedin.com/in/ethan-ehrler-b5855a251/">LinkedIn</a>.</p>
+                        <p>Vous pouvez me contacter par mail à <a href="mailto: ethan.ehrler@outlook.com">ethan.ehrler@outlook.com</a>, par téléphone au <a href="tel: 06 16 52 07 25">06 16 52 07 25</a> ou via <a target="_blank" href="https://www.linkedin.com/in/ethan-ehrler-b5855a251/">LinkedIn</a>.</p>
                     </div>
                 </div>
             </div>
