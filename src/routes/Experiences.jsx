49e56d9c660fc427192bd79ff9fe5ddf3689d409
Erlow <erlowgaming@gmail.com>
2024-05-05 18:09:45 +0200

    fix

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -14,7 +14,7 @@ function Experiences() {
 
                 <div className="timeline">
                     <div className="entries">
-                        <Timeline image={"../assets/logo/sercel.png"} title={"Sercel"} date={"2023-2024"} description={"Alternance développeur Web au sein de Sercel Grenoble."} />
+                        <Timeline image={"../assets/logo/sercel.png"} title={"Sercel"} date={"2023-2024"} description={"Alternant développeur web au sein de Sercel Grenoble."} />
                         <Timeline image={"../assets/logo/lig.jpg"} title={"Laboratoire informatique de Grenoble (LIG)"} date={"2023"} description={"Stagiaire développeur web au LIG. Extension de l'outil LOMET en support à l'ingénierie des méthodes."} />
                         <Timeline image={"../assets/logo/IUT2.jpeg"} title={"BUT informatique"} date={"2021-2024"} description={"Formation dans le domaine de l'informatique à Grenoble au sein de l'IUT 2, UGA."} />
                     </div>
