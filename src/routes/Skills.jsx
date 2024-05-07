8d9d8198a0ac5cf13891f5fba1087a45d498f3f6
Erlow <erlowgaming@gmail.com>
2024-05-07 15:32:02 +0200

    add next

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -39,6 +39,10 @@ function Skills() {
                         React
                       </p>
                       <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 15 15"><path fill="currentColor" fill-rule="evenodd" d="M0 7.5a7.5 7.5 0 1 1 11.698 6.216L4.906 4.21A.5.5 0 0 0 4 4.5V12h1V6.06l5.83 8.162A7.5 7.5 0 0 1 0 7.5M10 10V4h1v6z" clip-rule="evenodd"/></svg>
+                        Next.js
+                      </p>
+                      <p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="#dd0031" d="M16 2L2.966 6.648l1.988 17.234L16 30l11.046-6.118l1.988-17.234z"/><path fill="#c3002f" d="M16 2v3.108v-.014V30l11.046-6.118l1.988-17.234z"/><path fill="#fff" d="m16 5.094l-8.148 18.27h3.038l1.638-4.088h6.916l1.638 4.088h3.038zm2.38 11.662h-4.76L16 11.03Z"/></svg>
                         Angular
                       </p>
