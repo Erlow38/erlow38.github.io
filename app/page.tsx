29971330451c23e17dba6f20b97438e3e770e36c
Erlow <erlowgaming@gmail.com>
2025-11-09 23:41:47 +0100

    Remove CustomCursor component from Home page and update cursor styling in globals.css for desktop view.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Contact from '@/components/Contact'
 import Navigation from '@/components/Navigation'
-import CustomCursor from '@/components/CustomCursor'
 import InteractiveSkills from '@/components/InteractiveSkills'
 import BentoProjects from '@/components/BentoProjects'
 import ExperienceTimeline from '@/components/ExperienceTimeline'
@@ -12,7 +11,7 @@ import ExperienceTimeline from '@/components/ExperienceTimeline'
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <CustomCursor />
+      {/* <CustomCursor /> */}
       <Navigation />
       <Hero />
       <About />
